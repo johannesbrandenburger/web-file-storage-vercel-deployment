d1447f151bbf729d98c964fa2681af323619ee26
Fabian <fabian.klimpel2002@gmail.com>
2023-10-24 15:18:53 +0200

    azure blob file download test

--- app/api/test-azure-blob/route.ts
+++ app/api/test-azure-blob/route.ts
@@ -1,20 +1,36 @@
-import { BlobServiceClient, BlockBlobClient } from '@azure/storage-blob';
-import fs from 'fs';
+import { BlockBlobClient } from '@azure/storage-blob';
 
 export async function GET(request: Request) {
 
   console.log(process.env.AZURE_STORAGE_CONNECTION_STRING);
   let containerName = "web-file-storage";
   let blobName = "Johannes.jpg"
+  // @ts-ignore
   let blockBlobClient = new BlockBlobClient(process.env.AZURE_STORAGE_CONNECTION_STRING, containerName, blobName);
 
   let downloadBlockBlobResponse = await blockBlobClient.download(0);
 
-  let ws = fs.createWriteStream("Johannes.jpg");
-  if (downloadBlockBlobResponse.readableStreamBody) {
-    await downloadBlockBlobResponse.readableStreamBody.pipe(ws);
-    console.log("Downloaded blob content to file");
-  }  
+  if (downloadBlockBlobResponse.readableStreamBody != null) {
+    let data: any = await streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
+    return new Response(data, {
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': 'attachment; filename=' + "Johannes.jpg"
+      },
+    });
+  }
+  return new Response(null, { status: 404 });
+}
 
-  return new Response('Hello world!') 
+async function streamToBuffer(readableStream: NodeJS.ReadableStream) {
+  return new Promise((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      readableStream.on('data', (data: Buffer) => {
+          chunks.push(data);
+      });
+      readableStream.on('end', () => {
+          resolve(Buffer.concat(chunks));
+      });
+      readableStream.on('error', reject);
+  });
 }
